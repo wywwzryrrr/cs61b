33e3178cf52e886690fb07ddbd64c61b4eb46d15
zng.xee <zng.xee@gmail.com>
2025-04-01 15:52:17 +0800

    Resotred correct version from lab1commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,15 +5,13 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 ==0) {
+            n = n / 2;
+        }  else{
+            n = 3 * n + 1;
         }
+        return n;
     }
-    
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
