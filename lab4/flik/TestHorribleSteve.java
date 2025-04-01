13d90409a3247e2b76b1dfa8d4a0fd06311cd5a5
zng.xee <zng.xee@gmail.com>
2025-04-01 17:33:17 +0800

    fix some errors

--- lab4/flik/TestHorribleSteve.java
+++ lab4/flik/TestHorribleSteve.java
@@ -1,8 +1,13 @@
 package flik;
 
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 
 public class TestHorribleSteve {
     @Test
-
+    public void testFormat() {
+        assertTrue()
+    }
 }
