2bf55dfcb073bd5f885b58f4e1368271e6ab11d0
wywwzryrrr <zng.xee@gmail.com>
2025-04-11 10:56:23 +0800

    first commit

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -18,9 +18,11 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
 
+    // TODO Hint: look at the `join`
+    //      function in Utils
+    static final File CAPERS_FOLDER = join(CWD, ".capers");
+    static final File STORY = join(CAPERS_FOLDER, "story");
     /**
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
@@ -32,6 +34,7 @@ public class CapersRepository {
      */
     public static void setupPersistence() {
         // TODO
+
     }
 
     /**
