eebe5e172b3e4c0b515b07cbd699d27bbb6b510c
wywwzryrrr <zng.xee@gmail.com>
2025-05-07 17:06:50 +0800

    done implementing clear, containsKey, get, size

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -7,50 +7,83 @@ import java.util.Spliterator;
 import java.util.function.Consumer;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private class BSTNode {
+        private K item;
+        private V value;
+        private int size;
+        private BSTNode left, right;
+
+        public BSTNode(K item, V value, int size) {
+            this.item = item;
+            this.value = value;
+            this.size = size;
+        }
+    }
+
+    private BSTNode root;
 
     @Override
     public void clear() {
-
+        root = null;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        return getNode(root, key) != null;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (key == null) {
+            return null;
+        }
+        return getNode(root, key).value;
+    }
+
+    private BSTNode getNode(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.item) < 0) {
+            return getNode(node.left, key);
+        }else if (key.compareTo(node.item) > 0) {
+            return getNode(node.right, key);
+        } else {
+            return node;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        return root.size;
     }
 
     @Override
     public void put(K key, V value) {
-
+        
     }
 
     @Override
     public Set<K> keySet() {
-        return Set.of();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public V remove(K key) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public V remove(K key, V value) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -61,5 +94,9 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     @Override
     public Spliterator<K> spliterator() {
         return Map61B.super.spliterator();
+    }
+
+    public BSTNode printInOrder() {
+        return null;
     }
 }
