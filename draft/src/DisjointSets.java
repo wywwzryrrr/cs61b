6aba123aa52460b3f50ac0a890439918bf19077a
wywwzryrrr <zng.xee@gmail.com>
2025-05-13 10:05:31 +0800

    DisjointSets findWithPathCompression, connect

--- draft/src/DisjointSets.java
+++ draft/src/DisjointSets.java
@@ -10,42 +10,56 @@ public class DisjointSets {
         }
 
         private int find(int p) {
-            while (parent[p] >= 0) {
-                p = parent[p];
+            int root = p;
+            if (parent[root] < 0) {
+                return root;
+            } else {
+                return find(parent[root]);
             }
-            return p;
         }
 
         private int findWithPathCompression(int p) {
             int root = p;
-            while (root != parent[root]) {
-                root = parent[root];
+            if (parent[root] < 0) {
+                return root;
+            } else {
+                return parent[root] = findWithPathCompression(parent[root]);
             }
-            while (p != root) {
-                int newp = parent[p];
-                parent[p] = root;
-                p = newp;
-            }
-            return root;
         }
 
         public void connect(int p, int q) {
-            int i = find(p);
-            int j = find(q);
-            parent[i] = j;
+            int rootx = findWithPathCompression(p);
+            int rooty = findWithPathCompression(q);
+            if (rootx != rooty) { // 只有当根不同时才合并
+                // 比较子树大小（负值越小，子树越大）
+                if (parent[rooty] < parent[rootx]) { // rooty 的树更大
+                    parent[rootx] = rooty;
+                } else if (parent[rooty] > parent[rootx]) { // rootx 的树更大
+                    parent[rooty] = rootx;
+                } else { // 大小相等，选择 rooty 作为根并增加其大小
+                    parent[rootx] = rooty;
+                    parent[rooty]--; // 增加 rooty 的子树大小
+                }
+            }
         }
 
         public boolean isConnected(int p, int q) {
-            return find(p) == find(q);
+            return findWithPathCompression(p) == findWithPathCompression(q);
         }
     }
 
     public static void main(String[] args) {
-        QuickUnionDS a = new QuickUnionDS(5);
+       /* QuickUnionDS a = new QuickUnionDS(5);
         System.out.println(a.find(0));
         System.out.println(a.find(1));
         a.connect(1, 0);
         System.out.println(a.parent[0]);
-        System.out.println(a.parent[1]);
+        System.out.println(a.parent[1]);*/
+        QuickUnionDS b = new QuickUnionDS(5);
+        System.out.println(b.findWithPathCompression(4));
+        System.out.println(b.findWithPathCompression(1));
+        b.connect(1, 4);
+        System.out.println(b.parent[1]);
+        System.out.println(b.parent[4]);
     }
 }
