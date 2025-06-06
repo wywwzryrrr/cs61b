6b56273785378878cf68c4bb163e05194196f83f
wywwzryrrr <zng.xee@gmail.com>
2025-06-06 16:13:16 +0800

    lab8

--- skeleton-sp21/lab8/hashmap/MyHashMap.java
+++ skeleton-sp21/lab8/hashmap/MyHashMap.java
@@ -7,7 +7,7 @@ import java.util.Collection;
  *  access to elements via get(), remove(), and put() in the best case.
  *
  *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author YOUR NAME HERE
+ *  @author zng.xee
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
