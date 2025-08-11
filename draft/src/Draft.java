31dd919d8afd1f6e2227128b880b270d726c1a0d
wywwzryrrr <zng.xee@gmail.com>
2025-08-11 14:45:00 +0800

    draft

--- draft/src/Draft.java
+++ draft/src/Draft.java
@@ -367,4 +367,50 @@ public class Draft {
     public static void main(String[] args) {
         mystery(6);
     }*/
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        // copy data to temp arr
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+        // merge the arrs
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        // copy the remaining elements
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    // recursive function
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left == right) return;
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {11, 223, 32, 30, 1, 300, 20, 9090, 49, 89, 26, 13};
+        mergeSort(arr, 0, arr.length - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
