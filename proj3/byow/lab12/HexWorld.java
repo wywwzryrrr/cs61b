81e74d9af26ddfd1638fdc8c4bae921e2b320e5a
wywwzryrrr <zng.xee@gmail.com>
2025-08-14 10:48:00 +0800

    lab12 done

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -56,7 +56,6 @@ public class HexWorld {
         // draw the first row
         Position start = p.shift(b, 0);
         drawRow(start, tile, tiles, t);
-
         // draw the remaining rows recursively
         if (b > 0) {
             Position next = p.shift(0, -1);
@@ -65,6 +64,24 @@ public class HexWorld {
         // draw the last row
         Position end = start.shift(0, -(2 * b + 1));
         drawRow(end, tile, tiles, t);
+    }
+
+    /**
+     * adds a column of NUM hexagons with the given size to the given position p,
+     * each of which biome is random
+     * @param p
+     * @param tiles
+     * @param size
+     */
+    public static void addHexColumn(Position p, TETile[][] tiles, int size, int num) {
+        if (num < 1) return;
+        // draw the first hexagon
+        addHexagon(p, tiles, size, randomTile());
+        // recursively draw the remaining hexagons
+        if (num > 1) {
+            Position bottomNeighbour = getBottomNeighbour(p, size);
+            addHexColumn(bottomNeighbour, tiles, size, num - 1);
+        }
     }
 
     /**
@@ -82,40 +99,83 @@ public class HexWorld {
         public Position shift(int x, int y) {
             return new Position(this.x + x, this.y + y);
         }
+
     }
 
-    /**
-     */
     private static TETile randomTile() {
         int tileNum = RANDOM.nextInt(9);
         switch (tileNum) {
             case 0: return Tileset.WALL;
             case 1: return Tileset.FLOWER;
-            case 2: return Tileset.NOTHING;
-            case 3: return Tileset.WATER;
-            case 4: return Tileset.MOUNTAIN;
-            case 5: return Tileset.AVATAR;
-            case 6: return Tileset.SAND;
-            case 7: return Tileset.GRASS;
-            case 8: return Tileset.TREE;
+            case 2: return Tileset.WATER;
+            case 3: return Tileset.MOUNTAIN;
+            case 4: return Tileset.AVATAR;
+            case 5: return Tileset.SAND;
+            case 6: return Tileset.GRASS;
+            case 7: return Tileset.TREE;
+            case 8: return Tileset.FLOOR;
             default: return Tileset.NOTHING;
         }
     }
 
     /**
+     * get the position of the bottom neighbour of a hexagon at Position P
+     * SIZE is the size of the hexagon
+     * @param p
+     * @param size
+     * @return
+     */
+    public static Position getBottomNeighbour(Position p, int size) {
+        return p.shift(0, -2 * size);
+    }
+
+    /**
+     * get the position of the top right neighbour of a hexagon at Position P
+     * SIZE is the size of the hexagon
+     * @param p
+     * @param size
+     * @return
+     */
+    public static Position getTopRightNeighbour(Position p, int size) {
+        return p.shift(2 * size - 1, size);
+    }
+
+    /**
+     * get the position of the bottom right neighbour of a hexagon at Position P
+     * SIZE is the size of the hexagon
+     * @param p
+     * @param size
+     * @return
+     */
+    public static Position getBottomRightNeighbour(Position p, int size) {
+        return p.shift(2 * size - 1, -size);
+    }
+
+    /**
      * draw the hexagonal world
      */
-    public static void drawWorld(TETile[][] tiles) {
-        fillBoardWithNothing(tiles);
-        Position p = new Position(20, 20);
-        addHexagon(p, tiles, 3, Tileset.FLOWER);
+    public static void drawWorld(TETile[][] tiles, Position p, int hexSize, int tessSize) {
+        // draw the first hexColumn
+        addHexColumn(p, tiles, hexSize, tessSize);
+        // expand up and to the right
+        for(int i = 1; i < tessSize; i++) {
+            p = getTopRightNeighbour(p, hexSize);
+            addHexColumn(p, tiles, hexSize, tessSize + i);
+        }
+        // expand down and to the right
+        for (int i = tessSize - 2; i >= 0 ; i--) {
+            p = getBottomRightNeighbour(p, hexSize);
+            addHexColumn(p, tiles, hexSize, tessSize + i);
+        }
     }
 
     public static void main(String[] args) {
         TERenderer ter = new TERenderer();
+        Position p = new Position(5, 30);
         ter.initialize(WIDTH, HEIGHT);
         TETile[][] world = new TETile[WIDTH][HEIGHT];
-        drawWorld(world);
+        fillBoardWithNothing(world);
+        drawWorld(world, p,3, 3);
         ter.renderFrame(world);
     }
 }
